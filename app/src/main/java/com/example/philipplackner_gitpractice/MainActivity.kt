8c26846025fa959226c201b59460e40880613616
sayuti <ahmad.sayuti@delameta.com>
2023-03-31 05:30:08 +0700

    This is second commit

--- app/src/main/java/com/example/philipplackner_gitpractice/MainActivity.kt
+++ app/src/main/java/com/example/philipplackner_gitpractice/MainActivity.kt
@@ -1,12 +1,13 @@
 package com.example.philipplackner_gitpractice
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.philipplackner_gitpractice.R
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        println("Second commit")
     }
 }
