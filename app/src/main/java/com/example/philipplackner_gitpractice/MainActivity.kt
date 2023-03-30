04ad3b148f4c812529dbf95981d25829e035411b
sayuti <ahmad.sayuti@delameta.com>
2023-03-31 06:14:27 +0700

    Experimental Commit

--- app/src/main/java/com/example/philipplackner_gitpractice/MainActivity.kt
+++ app/src/main/java/com/example/philipplackner_gitpractice/MainActivity.kt
@@ -9,5 +9,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         println("Second commit")
+        println("local changes")
+        println("experimental")
     }
 }
